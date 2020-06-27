3e58ce7b3fce5b903df1a9b677d633ed43c5e1d5
gentlymiss <1084663453@qq.com>
2020-06-27 12:02:13 +0800

    1

--- src/main/java/wh/practice/algorithm/ReverseListNode.java
+++ src/main/java/wh/practice/algorithm/ReverseListNode.java
@@ -48,4 +48,20 @@ public class ReverseListNode {
         head.next = null;
         return l;
     }
+
+
+    static class ListNode {
+        int val;
+        ListNode next;
+        ListNode(int x) {
+            val = x;
+        }
+
+        @Override
+        public String toString() {
+            return "{" + val +
+                    ", " + next +
+                    '}';
+        }
+    }
 }
